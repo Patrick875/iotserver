a5e8fe9e02bcbb9b1540af3830048163525573d9
Patrick875 <kubwimanapatrick@gmail.com>
2023-01-19 14:51:45 +0200

    check http for socket

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 const port = 3500;
-const server = require("https").createServer(app);
+const server = require("http").createServer(app);
 const io = require("socket.io")(server, { cors: { origin: "*" } });
 
 io.on("connection", (socket) => {
