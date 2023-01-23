bf5274562c95763f99a0ddac97fee5ead3ac7d8c
Patrick875 <kubwimanapatrick@gmail.com>
2023-01-23 12:45:32 +0200

    Still trying real time with hosted server

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const io = require("socket.io")(server, {
 		origin: "*",
 		methods: ["GET", "POST"],
 		allowedHeaders: ["my-custom-header"],
-		credentials: true,
+		withCredentials: true,
 	},
 });
 
