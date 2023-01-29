306be9ee8f1633a07cb97b49976dc7ffa7075374
Patrick875 <kubwimanapatrick@gmail.com>
2023-01-29 15:46:35 +0200

    websokects reverting to before netlify

--- app.js
+++ app.js
@@ -2,12 +2,11 @@
 const express = require("express");
 const cors = require("cors");
 const sensor = require("./api/sensor");
-const serverless = require("serverless-http");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/.netlify/functions/server", sensor);
+
+app.use("/api/sensor", sensor);
 
 module.exports = app;
-module.exports.handler = serverless(app);
